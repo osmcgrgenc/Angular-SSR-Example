f49ce70c61ba4e064e44fc1777a5b9e297d0b3ba
Osman Cagri GENC <osman.cagri@danisman.avansas.com>
2022-12-07 13:34:36 +0300

    first commit

---
+++ frontend/src/app/modules/application/example-prism/tutorial.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
+@Component({
+  selector: 'app-example-modal',
+  templateUrl: './tutorial.component.html',
+  styleUrls: ['./tutorial.component.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class TutorialComponent implements OnInit {
+
+  element = {
+    text: 'console.log("0001");',
+    languagecode: 'javascript'
+  }
+
+  constructor(@Inject(PLATFORM_ID) private platformId: object,) {
+  }
+
+  ngOnInit(): void {
+    this.loadScript('assets/params/js/prism.js');
+  }
+
+  loadScript(name: string): void {
+
+    if (isPlatformBrowser(this.platformId)) {
+      const src = document.createElement('script');
+      src.type = 'text/javascript';
+      src.src = name;
+      src.async = false;
+      document.getElementsByTagName('head')[0].appendChild(src);
+    }
+  }
+
+
+}
